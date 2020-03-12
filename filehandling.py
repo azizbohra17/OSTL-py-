828aeb82632c6dd1407b8cc0bbd1b902b9435e79
Aziz Bohra <47524749+azizbohra17@users.noreply.github.com>
2020-03-12 16:03:25 +0530

    Delete filehandling.py

--- filehandling.py
+++
@@ -1,8 +0,0 @@
-f = open("xyz.txt","r")
-w = c = l = 0
-for line in f:
-	l = l+1
-	words = line.split()
-	w = w+len(words)
-	c = c+len(line)
-print('Number of lines: ',l,'\nNumber of words:',w,'\nNumber of character:',c)
